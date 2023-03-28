ca973d653871e91de08c6bf222a7cb46ca2aa7b5
gustavo <derosasgustavo@gmail.com>
2023-03-28 00:54:24 -0300

    modificaciones menores

--- javascript.js
+++ javascript.js
@@ -2,9 +2,9 @@ let renderizado = document.getElementById("renderizado");
 let botonRunningAmateur = document.getElementById("botonRunningAmateur");
 let botonRunningProfesional = document.getElementById("botonRunningProfesional");
 let botonRunningMontana = document.getElementById("botonRunningMontana")
-let zapatillaRunningAmateur = `../imagenes/zapatillaRunningAmateur.jpg`
-let zapatillaRunningProfesional = `../imagenes/zapatillaRunningProfesional.jpg`
-let zapatillasRunningMontana = `../imagenes/zapatillasRunningMontana.jpg`
+let zapatillaRunningAmateur = `./imagenes/zapatillaRunningAmateur.jpg`
+let zapatillaRunningProfesional = `./imagenes/zapatillaRunningProfesional.jpg`
+let zapatillasRunningMontana = `./imagenes/zapatillasRunningMontana.jpg`
 
 function renderizar (boton,zapatilla) {
   boton.onclick  = () => {   
